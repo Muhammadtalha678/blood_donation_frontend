2507cd6a1950871b0137b8949866050b4c31fe5f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-09 10:50:35 +0500

    change link from href to to

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -35,7 +35,8 @@ const Header = () => {
                 
                 return <Link
                   key={item.name}
-                  href={item.href}
+                  to={item.href}
+                  // href={item.href}
                   className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 hover:text-gray-900 hover:border-red-500"
                 >
                   {item.name}
